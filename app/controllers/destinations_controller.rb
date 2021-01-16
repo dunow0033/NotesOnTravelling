9fc1cf0dcb01ceef0aecba3a670abd89e83e49b8
dunow <dunow33@hotmail.com>
2021-01-16 12:47:26 -0500

    filling out some more files with pre-code instructions

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -3,34 +3,8 @@ class DestinationsController < ApplicationController
         @destinations = Destination.all
     end
 
-    def new
-        @destination = Destination.new
-    end
-
-    def create
-        @destination = Destination.create(destination_params)
-        redirect_to destination_path(@destination)
-    end
-
     def show
         @destination = Destination.find_by(id: params[:id])
         @note = @destination.notes.build(user_id: current_user.id)
     end
-
-    def edit
-        @destination = Destination.find_by(id: params[:id])
-        @note = @destination.notes.build(user_id: current_user.id)
-    end
-
-    def update
-        @destination = Destination.find_by(id: params[:id])
-        @destination.update(destination_params)
-        redirect_to destination_path(@destination)
-    end
-
-    private 
-
-    def attraction_params 
-        params.require(:attraction).permit(:name, :min_height, :tickets, :happiness_rating, :nausea_rating)
-    end 
 end
