f826129b9dda258cbaebff0d912e8a5d150127c0
dunow <dunow33@hotmail.com>
2021-02-01 01:44:02 -0500

    I think I got the error message correctly displaying for blank note content

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
             session[:user_id] = user.id
             redirect_to destinations_path
         else
-            redirect_to '/users/new'
+            render :new
         end
     end
 
